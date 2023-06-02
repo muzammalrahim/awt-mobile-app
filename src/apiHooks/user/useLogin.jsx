114cdaf4bbc190e2f45585f0310a8fe60ca3b012
muzammalrahim <creative.joomdev@gmail.com>
2023-06-02 10:57:15 +0500

    Login Functionality Add

--- src/apiHooks/user/useLogin.jsx
+++ src/apiHooks/user/useLogin.jsx
@@ -4,35 +4,38 @@ import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../../context/UserContext";
 const useLogin = () => {
-  const { setuser } = useUserContext();
-  const api = useAxios();
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+ const { setuser } = useUserContext();
+ const api = useAxios();
+ const navigate = useNavigate();
+ const [loading, setLoading] = useState(false);
+ const [error, setError] = useState(null);
 
-  const loginUser = async (credentials) => {
-    setLoading(true);
-    setError(null);
-    try {
-      const { data, status } = await api.post("/new-login", credentials);
-      if (status === 200) {
-        console.log(data);
-        // Store the token in local storage or session storage
-        localStorage.setItem("token", data?.token);
-        localStorage.setItem("user", JSON.stringify(data?.user));
-        setuser(data?.user);
-        toast.success(data?.message);
-        navigate("/"); // Navigate to the dashboard or desired page after successful login
-      }
-    } catch (error) {
-      setError(error.response.data.message || "Login failed");
-      toast.error(error.response.data.message || "Login failed");
+ const loginUser = async (credentials) => {
+  setLoading(true);
+  setError(null);
+  try {
+   const { data, status } = await api.post("/new-login", credentials);
+   if (status === 200) {
+    if (data?.user?.role !== "Resident") {
+     return toast.error("User Not Found, Please Login Again");
     }
+    console.log(data);
+    // Store the token in local storage or session storage
+    localStorage.setItem("token", data?.token);
+    localStorage.setItem("user", JSON.stringify(data?.user));
+    setuser(data?.user);
+    toast.success(data?.message);
+    navigate("/"); // Navigate to the dashboard or desired page after successful login
+   }
+  } catch (error) {
+   setError(error.response.data.message || "Login failed");
+   toast.error(error.response.data.message || "Login failed");
+  }
 
-    setLoading(false);
-  };
+  setLoading(false);
+ };
 
-  return loginUser;
+ return loginUser;
 };
 
 export default useLogin;
--- src/complain/complain-table.jsx
+++ src/complain/complain-table.jsx
@@ -5,35 +5,28 @@ import { Route, useNavigate } from "react-router-dom";
 import { Button } from "@material-tailwind/react";
 
 const ComplainTable = ({ headings, complains, show, setshow }) => {
-  const navigate = useNavigate();
-  const handleAddComplain = () => {
-    navigate("/complain");
-  };
-  return (
-    <>
-      <Navbar />
-      <div className="p-5">
-        <Button
-          variant="outlined"
-          color="green"
-          className="w-full h-10 "
-          onClick={handleAddComplain}
-        >
-          Add new complain
-        </Button>
-      </div>
-      <div className="px-5">
-        <div className="flex flex-col">
-          <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-              <div className="overflow-hidden">
-                {!complains && <span>Loading...</span>}
-                {complains?.length === 0 && (
-                  <span className="text-3xl text-gray-700">
-                    No Complains Found
-                  </span>
-                )}
-                {/* <table className="min-w-full text-left text-sm font-light">
+ const navigate = useNavigate();
+ const handleAddComplain = () => {
+  navigate("/complain");
+ };
+ return (
+  <>
+   <Navbar />
+   <div className="p-5">
+    <Button variant="outlined" color="green" className="w-full h-10 " onClick={handleAddComplain}>
+     Add new complaint
+    </Button>
+   </div>
+   <div className="px-5">
+    <div className="flex flex-col">
+     <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+       <div className="overflow-hidden">
+        {!complains && <span>Loading...</span>}
+        {complains?.length === 0 && (
+         <span className="text-3xl text-gray-700">No Complains Found</span>
+        )}
+        {/* <table className="min-w-full text-left text-sm font-light">
                   <thead className="dark:border-neutral-500 border-b font-medium">
                     <tr>
                       {headings?.map((e) => (
@@ -92,65 +85,56 @@ const ComplainTable = ({ headings, complains, show, setshow }) => {
                   </tbody>
                 </table> */}
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                  {complains?.map((item) => (
-                    <div
-                      key={item?._id}
-                      className="bg-gray-100 dark:bg-neutral-900 shadow rounded-lg p-4 border-t-2 border-b-2 border-gray-200"
-                    >
-                      <div className="flex items-baseline gap-5 text-[24px] ">
-                        <div className="flex items-center justify-center w-12 h-12 bg-white text-gray-600 text-xl font-bold  mb-4">
-                          {item.subcategory.name.charAt(0)}
-                        </div>
-                        <div className="font-bold">{item.subcategory.name}</div>
-                        <div className="">
-                          <div className="flex justify-end text-[16px]">
-                            ({item?.status?.name})
-                          </div>
-                          <div className="flex justify-end text-black-500 text-[14px]">
-                            {formatDistanceToNow(new Date(item.date), {
-                              addSuffix: false,
-                            })}
-                          </div>
-                        </div>
-                      </div>
-                      <div className="text-black">
-                        {item.description?.substring(0, 40)}
-                      </div>
-
-                      {show.id === item._id && show.display && (
-                        <div className="text-black">
-                          {item.description?.substring(40)}
-                        </div>
-                      )}
-
-                      {item.description.length > 40 && (
-                        <div
-                          onClick={() =>
-                            setshow((prev) => ({
-                              ...prev,
-                              display:
-                                show.id === item._id ? !show.display : true,
-                              id: item._id,
-                            }))
-                          }
-                          className="text-sm text-blue-800 cursor-pointer"
-                        >
-                          {show.id === item._id && show.display
-                            ? "See Less"
-                            : "...See More"}
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+         {complains?.map((item) => (
+          <div
+           key={item?._id}
+           className="bg-gray-100 dark:bg-neutral-900 shadow rounded-lg p-4 border-t-2 border-b-2 border-gray-200"
+          >
+           <div className="flex items-baseline gap-5 text-[24px] ">
+            <div className="flex items-center justify-center w-12 h-12 bg-white text-gray-600 text-xl font-bold  mb-4">
+             {item.subcategory.name.charAt(0)}
             </div>
+            <div className="font-bold">{item.subcategory.name}</div>
+            <div className="">
+             <div className="flex justify-end text-[16px]">({item?.status?.name})</div>
+             <div className="flex justify-end text-black-500 text-[14px]">
+              {formatDistanceToNow(new Date(item.date), {
+               addSuffix: false,
+              })}
+             </div>
+            </div>
+           </div>
+           <div className="text-black">{item.description?.substring(0, 40)}</div>
+
+           {show.id === item._id && show.display && (
+            <div className="text-black">{item.description?.substring(40)}</div>
+           )}
+
+           {item.description.length > 40 && (
+            <div
+             onClick={() =>
+              setshow((prev) => ({
+               ...prev,
+               display: show.id === item._id ? !show.display : true,
+               id: item._id,
+              }))
+             }
+             className="text-sm text-blue-800 cursor-pointer"
+            >
+             {show.id === item._id && show.display ? "See Less" : "...See More"}
+            </div>
+           )}
           </div>
+         ))}
         </div>
+       </div>
       </div>
-    </>
-  );
+     </div>
+    </div>
+   </div>
+  </>
+ );
 };
 
 export default ComplainTable;
